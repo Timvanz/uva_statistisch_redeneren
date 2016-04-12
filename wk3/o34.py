a96a301f5ceaf387ad07e25683ff0da33648f843
Maico Timmerman <maico.timmerman@gmail.com>
2016-04-12 22:31:54 +0200

    Added lambda to the inverse sample

--- wk3/o34.py
+++ wk3/o34.py
@@ -2,17 +2,17 @@
 # Authors:  Tim van Zalingen (10784012)
 #           Maico Timmerman (10542590)
 # Date:     12 April 2016
-# File: 21.py
+# File: 34.py
 #
-# The file for assignment 3.4. Plots N number pairs using
-# exponential distribution.
+# The file for assignment 3.4/3.5. Plots N number pairs using
+# exponential distribution. Show the distribution in a histogram.
 # -------------------------------------------------------------
 from o22 import ibm_rnd
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def exp_rnd(N, seed):
+def exp_rnd(N, seed, _lambda):
     """
     Calculate N random numbers using seed
     and given lambda.
@@ -23,17 +23,20 @@ def exp_rnd(N, seed):
         Inverse transform sampling for the exponential
         distribution
         """
-        return -np.log(1-x)
+        return (-np.log(1-x)) / _lambda
 
     # Generate N random numbers using normal distribution.
     nums = ibm_rnd(N, seed)
     return np.vectorize(vec_exp)(nums)
 
 
+def lambda_estimation():
+    pass
+
 if __name__ == '__main__':
     N = 1000
-    x = exp_rnd(N, 983)
-    y = exp_rnd(N, 759)
+    x = exp_rnd(N, 983, 0.5)
+    y = exp_rnd(N, 759, 0.5)
     plt.title('{0} number (exponential distributed) pairs created with IBM RND'
               .format(N))
     plt.subplot(1, 2, 1)
