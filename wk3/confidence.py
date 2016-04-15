2c66c29616b056edef0b3188c1c060e4b52a079d
Maico Timmerman <maico.timmerman@gmail.com>
2016-04-15 12:49:10 +0200

    Removed whitelines

--- wk3/confidence.py
+++ wk3/confidence.py
@@ -15,11 +15,9 @@ def test_sample(data_mu, sample, interval):
 
 
 def main():
-    # random.seed('1')
     data = np.genfromtxt('./tijden-medium.log', dtype=[('time', float)])
     data_mu = np.mean(data['time'])
     interval = spst.t.interval(SAMPLE_PROBABILITY, SAMPLE_SIZE)
-
     hits = 0
     for _ in range(SAMPLE_NUM):
         if test_sample(data_mu,
@@ -30,7 +28,6 @@ def main():
     print("Estimate probability: %f" % SAMPLE_PROBABILITY)
     print("Actual probability of %d samples of size %d: %f" %
           (SAMPLE_NUM, SAMPLE_SIZE, (float(hits)/SAMPLE_NUM)))
-
 
 if __name__ == '__main__':
     main()
