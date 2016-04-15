34272369fbeec09758f20103ab44aa4d20e41972
Maico Timmerman <maico.timmerman@gmail.com>
2016-04-15 12:47:08 +0200

    Finished the confidence code

---
+++ wk3/confidence.py
@@ -0,0 +1,36 @@
+import numpy as np
+import scipy.stats as spst
+import random
+
+SAMPLE_NUM = 100
+SAMPLE_SIZE = 50
+SAMPLE_PROBABILITY = 0.95
+
+
+def test_sample(data_mu, sample, interval):
+    sample_mu = np.mean(sample)
+    sample_std = np.std(sample)
+    Z = (sample_mu - data_mu) / (sample_std / np.sqrt(SAMPLE_SIZE))
+    return (Z >= interval[0] and Z <= interval[1])
+
+
+def main():
+    # random.seed('1')
+    data = np.genfromtxt('./tijden-medium.log', dtype=[('time', float)])
+    data_mu = np.mean(data['time'])
+    interval = spst.t.interval(SAMPLE_PROBABILITY, SAMPLE_SIZE)
+
+    hits = 0
+    for _ in range(SAMPLE_NUM):
+        if test_sample(data_mu,
+                       np.asarray(random.sample(data['time'], SAMPLE_SIZE)),
+                       interval):
+            hits += 1
+
+    print("Estimate probability: %f" % SAMPLE_PROBABILITY)
+    print("Actual probability of %d samples of size %d: %f" %
+          (SAMPLE_NUM, SAMPLE_SIZE, (float(hits)/SAMPLE_NUM)))
+
+
+if __name__ == '__main__':
+    main()
