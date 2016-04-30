9bcf7f14290b5bc9621318fc08f198fc47859275
Tim <timvzalingen@gmail.com>
2016-04-30 20:07:44 +0200

    lab exercises af, report 19 t/m 22 af

--- wk4/lab_21.py
+++ wk4/lab_21.py
@@ -1,32 +1,47 @@
+# -----------------------------------------------------------------------------
+# Authors:  Tim van Zalingen (10784012)
+#           Maico Timmerman (10542590)
+#
+# Date:     30 april 2016
+#
+# File: lab_21.py
+#
+# This file generates random samples as a data matrix from the multivariate
+# normal distribution. 12 scatter plots are made for each position in the
+# matrix, exctept for the diagonal.
+# -----------------------------------------------------------------------------
 
 import pylab as pl
 
 if __name__=='__main__':
-    mu = pl.array([[2], [8], [16], [32]])
-    print mu
-    Sigma = pl.array([[6,2,2,6],[1,4,4,1],[10,1,10,1], [1,4,1,8]])
+    mu = pl.array([[0], [12], [24], [36]])
+    Sigma = pl.array([[ 3.01602775,  1.02746769, -3.60224613, -2.08792829],
+                      [ 1.02746769,  5.65146472, -3.98616664,  0.48723704],
+                      [-3.60224613, -3.98616664, 13.04508284, -1.59255406],
+                      [-2.08792829,  0.48723704, -1.59255406,  8.28742469]])
+    
+    """ The data matrix is created for above mu and Sigma. """
     d, U = pl.eig(Sigma)
     L = pl.diagflat(d)
     A = pl.dot(U, pl.sqrt(L))
     X = pl.randn(4, 1000)
-    print X
+    """ Y is the data matrix of random samples. """
     Y = pl.dot(A, X) + pl.tile(mu, 1000)
+    
     pl.figure(1)
     pl.clf()
-    
-    pl.plot(X[0], Y[2], '+', color='#FF0000')
-    pl.plot(X[0], Y[3], '+', color='#00FF00')
-    pl.plot(X[0], Y[1], '+', color='#0000FF')
-    pl.plot(X[1], Y[0], 'x', color='#FFFF00')
-    pl.plot(X[1], Y[2], 'x', color='#00FFFF')
-    pl.plot(X[1], Y[3], 'x', color='#444444')
-    pl.plot(X[2], Y[0], '.', color='#FFFFFF')
-    pl.plot(X[2], Y[1], '.', color='#222222')
-    pl.plot(X[2], Y[3], '.', color='#AAAAAA')
-    pl.plot(X[3], Y[0], '+', color='#FFAA22')
-    pl.plot(X[3], Y[1], '+', color='#22AAFF')
-    pl.plot(X[3], Y[2], '+', color='#FFDD00')
-    
-    pl.axis('equal')
-    pl.show()
+    pl.plot(X[0], Y[1], '+', color='#0000FF', label='i=0,j=1')
+    pl.plot(X[0], Y[2], '+', color='#FF0000', label='i=0,j=2')
+    pl.plot(X[0], Y[3], '+', color='#00FF00', label='i=0,j=3')
+    pl.plot(X[1], Y[0], 'x', color='#FFFF00', label='i=1,j=0')
+    pl.plot(X[1], Y[2], 'x', color='#00FFFF', label='i=1,j=2')
+    pl.plot(X[1], Y[3], 'x', color='#444444', label='i=1,j=3')
+    pl.plot(X[2], Y[0], '.', color='#774411', label='i=2,j=0')
+    pl.plot(X[2], Y[1], '.', color='#222222', label='i=2,j=1')
+    pl.plot(X[2], Y[3], '.', color='#AAAAAA', label='i=2,j=3')
+    pl.plot(X[3], Y[0], '+', color='#FFAA22', label='i=3,j=0')
+    pl.plot(X[3], Y[1], '+', color='#22AAFF', label='i=3,j=1')
+    pl.plot(X[3], Y[2], '+', color='#FFDD00', label='i=3,j=2')
+    pl.legend()
+    pl.savefig('fig21.png')
     
--- wk4/lab_22.py
+++ wk4/lab_22.py
@@ -1,11 +1,26 @@
+# -----------------------------------------------------------------------------
+# Authors:  Tim van Zalingen (10784012)
+#           Maico Timmerman (10542590)
+#
+# Date:     30 april 2016
+#
+# File: lab_22.py
+#
+# This file generates random samples as a data matrix from the multivariate
+# normal distribution. For different sizes of the data matrix, the average
+# covariance deviation and mean deviation is calculated for multiple runs.
+# Then, for fixed N, the covariance of the covariance matrix after multiple
+# runs is calculated.
+# -----------------------------------------------------------------------------
 
 import pylab as pl
-#import numpy
 
 if __name__=='__main__':
     mu = pl.array([[2], [8], [16], [32]])
-    #print mu
-    Sigma = pl.array([[6,2,2,6],[1,4,4,1],[10,1,10,1], [1,4,1,8]])
+    Sigma = pl.array([[ 3.01602775,  1.02746769, -3.60224613, -2.08792829],
+                      [ 1.02746769,  5.65146472, -3.98616664,  0.48723704],
+                      [-3.60224613, -3.98616664, 13.04508284, -1.59255406],
+                      [-2.08792829,  0.48723704, -1.59255406,  8.28742469]])
     d, U = pl.eig(Sigma)
     L = pl.diagflat(d)
     A = pl.dot(U, pl.sqrt(L))
@@ -13,17 +28,20 @@ if __name__=='__main__':
     N = []
     mu_deviations = []
     Sigma_deviations = []
+    
+    """ First part of the exercise. """
+    """ This loop is used to get different sizes of N. """
     for i in range(1, 40):
         means = pl.array([])
         covariances = pl.array([])
         N.append(50 * i)
-        for j in range(1, 200):
+        """ From this loop, the average is taken to get an accurate measurement. """
+        for _ in range(1, 200):
             X = pl.randn(4, 50 * i)
             Y = pl.dot(A, X) + pl.tile(mu, 50 * i)
             mean = pl.mean(Y, axis=1)
             covariance = pl.cov(Y)
             covariance = covariance.reshape((1,16))
-            #print covariance
             if (len(means) == 0 and len(covariances) == 0):
                 means = mean
                 covariances = covariance
@@ -32,16 +50,30 @@ if __name__=='__main__':
                 covariances = pl.vstack((covariances, covariance))
         mu_deviations.append(pl.mean(pl.std(covariances, axis=0)))
         Sigma_deviations.append(pl.mean(pl.std(means, axis=0)))
-            #print covariances
-    #print N
-            #print pl.mean(Y, axis=1)
-            #print pl.cov(Y)
-    
     
     pl.figure(1)
     pl.clf()
-    pl.plot(N, mu_deviations, label="average mean deviation")
-    pl.plot(N, Sigma_deviations, label="average covariance deviation")
+    pl.title('The average deviation, over 200 times,\n of the mean and covariance matrix for a given N')
+    pl.xlabel('N')
+    pl.ylabel('average deviation')
+    pl.plot(N, mu_deviations, label='average mean deviation')
+    pl.plot(N, Sigma_deviations, label='average covariance deviation')
     pl.legend()
-    pl.show()
+    pl.savefig('fig22.png')
     
+    """ Second part of the exercise. """
+    covariances = pl.array([])
+    """ Over the loop is iterated to create a data matrix of the covariances
+        of the data matrices obtained from the multivariate normal
+        distribution. The covariance from this data matrix of covariances is
+        shown. """
+    for _ in range(1, 200):
+        X = pl.randn(4, 1000)
+        Y = pl.dot(A, X) + pl.tile(mu, 1000)
+        covariance = pl.cov(Y)
+        if (len(covariances) == 0):
+            covariances = covariance
+        else:
+            covariances = pl.hstack((covariances, covariance))
+    covariance_data = pl.cov(covariances)
+    print covariance_data
